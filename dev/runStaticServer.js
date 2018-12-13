57c507dd37cb413ba5e97ccb0ac9a654c5a705b6
TonySergio <slayerid.montano@gmail.com>
2018-12-13 18:51:49 +0300

    static build server

--- dev/runStaticServer.js
+++ dev/runStaticServer.js
@@ -5,7 +5,7 @@ var server = new StaticServer({
   rootPath: path.join(__dirname, '..', 'dist'),            // required, the root of the server file tree
   port: 1333,               // required, the port to listen
   name: 'cms-configurator-http-server',   // optional, will set "X-Powered-by" HTTP header
-  host: '127.0.0.1',       // optional, defaults to any interface
+  //host: '127.0.0.1',       // optional, defaults to any interface
   cors: '*',                // optional, defaults to undefined
   followSymlink: true,      // optional, defaults to a 404 error
   templates: {
